f0c23205add496312d07bd2f4398899c22403e36
abder222 <abderounis04@gmail.com>
2025-04-29 00:35:51 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -34,15 +34,38 @@ const App = () => {
                         }        
                        
                         {
-                            /* Add ExpenseList component here */
+                             <div className='col-sm'>
+                        <ExpenseTotal />
+                    </div>
+
                         }         
 
                         {
-                            /* Add ExpenseItem component here */
+                          <h3 className='mt-3'>Allocation</h3>
+                    <div className='row '>
+                        <div className='col-sm'>
+                            <ExpenseList />
+                        </div>
+                    </div>
+  
                         }        
 
                         {
-                            /* Add AllocationForm component here under */
+                           <div className='col-sm'>
+                        <ExpenseItem />
+                    </div>
+                    {
+                           <h3 className='mt-3'>Change allocation</h3>
+                    <div className='row mt-3'>
+                        <div className='col-sm'>
+                            <AllocationForm />
+                        </div>
+                    </div>
+
+                    }
+
+                
+
                         }        
 
                 </div>
