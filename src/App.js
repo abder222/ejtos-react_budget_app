316dfb36ae4dc753d04f8a2de758dda014d35817
abder222 <abderounis04@gmail.com>
2025-05-03 19:19:06 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,30 +8,35 @@ import AllocationForm from './components/AllocationForm';
 import { AppProvider } from './context/AppContext';
 
 function App() {
-    return (
+    return ()
         <AppProvider>
             <div className='container'>
                 <h1 className='mt-3'>Company's Budget Allocation</h1>
-                <div className='row mt-3'>
+                <div className='row mt-3'>}
+        {
                     <div className='col-sm'>
                         <Budget />
                     </div>
-                    <div className='col-sm'>
+}
+
+                     {  <div className='col-sm'>
                         <Remaining />
                     </div>
-                    <div className='col-sm'>
+                     }
+                  
+{          <div className='col-sm'>
                         <ExpenseTotal />
                     </div>
                 </div>
-
-                <h3 className='mt-3'>Allocation</h3>
+}
+    {   <h3 className='mt-3'>Allocation</h3>
                 <div className='row '>
                     <div className='col-sm'>
                         <ExpenseList />
                     </div>
                 </div>
-
-                <h3 className='mt-3'>Change allocation</h3>
+    }
+{   <h3 className='mt-3'>Change allocation</h3>
                 <div className='row mt-3'>
                     <div className='col-sm'>
                         <AllocationForm />
@@ -39,7 +44,7 @@ function App() {
                 </div>
             </div>
         </AppProvider>
-    );
-}
+  };
+
 
 export default App;
